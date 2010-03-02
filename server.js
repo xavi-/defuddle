xavi-/defuddle

commit 452781ec44a31a181a26f7bb209fd7e6a259f443

Moved logic for the block game into a js file that can be on both the server and client side.

server.js
@@ -58,6 +58,8 @@ srv.urls["/tic-tac-toe.html"] = StaticFileHandler("./tic-tac-toe.html", "text/ht
 
 srv.urls["/game-lib/tic-tac-toe.js"] = StaticFileHandler("./game-lib/tic-tac-toe.js", "application/x-javascript");
 
+srv.urls["/game-lib/block.js"] = StaticFileHandler("./game-lib/block.js", "application/x-javascript");
+
 // /channel/<session-id>/send?msg=<json> => returns an info-id
 // /channel/<session-id>/read?info-id=<int-id> => returns a list of json messages
 var chn = (function() {
@@ -229,10 +231,17 @@ var chn = (function() {
 })();
 
 chn.onCreate(function(id, channel) {
-    if(id === "pictionary") { 
-        channel.onReceive(function(msg) { if("clear" in msg.content) { channel.data = channel.data.splice(-1); } });
-    } else if(id === "tic-tac-toe") { createTicTacToeGame(channel); }
+    if(id === "pictionary") { createBlockGame(channel); }
+    else if(id === "tic-tac-toe") { createTicTacToeGame(channel); }
 });
+
+function createBlockGame(channel) {
+    var blk = require("./game-lib/block");
+    
+    channel.onReceive(function(msg, sendMoreInfo) {
+        if("clear" in msg.content) { channel.data = channel.data.splice(-1); }
+    });
+}
 
 function createTicTacToeGame(channel) {
     var ttt = require("./game-lib/tic-tac-toe");

block-game.html
@@ -1,7 +1,8 @@
 <html>
 <head>
 <style>
-    #board { position: relative; border: thin solid black; }
+    #board-container { border: thin solid black; float: left; }
+    .board { position: relative; }
     .block { position: absolute; width: 20px; height: 20px; }
     .green { background-color: green; }
     .blue { background-color: blue; }
@@ -9,289 +10,51 @@
 </style>
 </head>
 <body>
-<div id="board"></div>
+<div id="board-container"></div>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js" type="text/javascript"></script>
+<script src="/game-lib/block.js" type="text/javascript"></script>
 <script>
-var Game = (function() {
-    function Event(ctx) {
-        var listeners = [];
-        
-        this.add = function(listener) { listeners.push(listener); };
-        
-        this.trigger = function trigger() {
-            for(var i = 0; i < listeners.length; i++) { listeners[i].call(ctx, ctx); }
-        };
-    }
-    
-    function Block(board, row, col, color) {
+(function() {
+    blk.Block.onCreate.add(function(block) {
         var div = document.createElement("div");
-        color = color || "";
         
-        this.color = function() { 
-            if(!arguments.length) { return color; }
-            
-            color = arguments[0];
-            
+        block.div = div;
+        
+        block.onColorChange.add(function(color) {
+            var pos = this.pos();
             div.className = "block " + color;
-            div.style.top = row * 20;
-            div.style.left = col * 20;
-        };
+            div.style.top = pos.row * 20;
+            div.style.left = pos.col * 20;
+        });
         
-        this.isEmpty = false;
+        block.onMove.add(function(e) {
+            div.style.top = e.row * 20;
+            div.style.left = e.col * 20;
+        });
         
-        this.pos = function() {
-            if(!arguments.length) { return { row: row, col: col }; }
-            
-            board[row][col] = board.emptyBlock(row, col);
-            if(isNaN(arguments[0])) {
-                row = arguments[0].row;
-                col = arguments[0].col;
-            } else if(!isNaN(arguments[0]) && !isNaN(arguments[1])) {
-                row = arguments[0];
-                col = arguments[1];
-            } else { return; }
-            
-            board[row][col].destroy();
-            board[row][col] = this;
-            
-            div.style.top = row * 20;
-            div.style.left = col * 20;
-        };
-        
-        this.destroy = function() {
-            if(row == null || col == null) { return; }
-            
-            board[row][col] = board.emptyBlock(row, col);
-            board.div.removeChild(div);
-            col = row = null;
-            color = "";
+        block.onDestroy.add(function() {
+            this.board.div.removeChild(div);
             div = null;
-            
-            return true;
-        };
+        });
         
-        div.className = "block " + color;
-        div.style.top = row * 20;
-        div.style.left = col * 20;
-        board.div.appendChild(div);
-        
-        board[row][col] = this;
-    }
-
-    function EmptyBlock(board, row, col) {    
-        this.color = function() {
-            if(!arguments.length) { return ""; }
-            
-            board[row][col] = new Block(board, row, col, arguments[0]);
-        }
-        
-        this.isEmpty = true;
-        
-        this.pos = function() { };
-        
-        this.destroy = function() { return false; };
-    }
-
-    function Board(rows, columns, div) {
-        this.div = div || document.createElement("div");
-
-        this.rows = rows;
-        this.columns = columns;
-        
-        this.emptyBlock = function(row, col) {
-            return new EmptyBlock(this, row, col);
-        };
-        
-        for(var r = -1; r <= rows; r++) {
-            this[r] = [];
-            
-            for(var c = -1; c <= columns; c++) {
-                this[r][c] = this.emptyBlock(r, c);
-            }
-        }
-    }
-    var Piece = (function() {
-        var colors = ["green", "blue", "red"];
-        
-        function Piece(board, blocks) {        
-            this.row = 0;
-            this.col = 4;
-            
-            this.isFalling = true;
-            
-            this.left = function left() {
-                var row = this.row, col = this.col;
-                
-                if(col - 1 < 0) { return; }
-                if(!board[row + 0][col - 1].isEmpty) { return; }
-                if(!board[row + 1][col - 1].isEmpty) { return; }
-                if(!board[row + 2][col - 1].isEmpty) { return; }
-                
-                board[row + 0][col].pos(row + 0, col - 1);
-                board[row + 1][col].pos(row + 1, col - 1);
-                board[row + 2][col].pos(row + 2, col - 1);
-                
-                this.col -= 1;
-            };
-            
-            this.rotate = function rotate() { 
-                var row = this.row, col = this.col;
-                
-                var tmp = board[row][col].color();
-                board[row][col].color(board[row + 1][col].color());
-                board[row + 1][col].color(board[row + 2][col].color());
-                board[row + 2][col].color(tmp);
-            };
-            
-            this.right = function right() {
-                var row = this.row, col = this.col;
-                
-                if(col + 1 >= board.columns) { return; }
-                if(!board[row + 0][col + 1].isEmpty) { return; }
-                if(!board[row + 1][col + 1].isEmpty) { return; }
-                if(!board[row + 2][col + 1].isEmpty) { return; }
-                
-                board[row + 0][col].pos(row + 0, col + 1);
-                board[row + 1][col].pos(row + 1, col + 1);
-                board[row + 2][col].pos(row + 2, col + 1);
-                
-                this.col += 1;
-            };
-            
-            this.down = function down() {
-                var row = this.row, col = this.col;
-                
-                this.isFalling = false;
-                if(row + 3 >= board.rows) { this.onPlacement.trigger(); return; }
-                if(!board[row + 3][col].isEmpty) { this.onPlacement.trigger(); return; }
-                this.isFalling = true;
-                                
-                board[row + 2][col].pos(row + 3, col);
-                board[row + 1][col].pos(row + 2, col);
-                board[row + 0][col].pos(row + 1, col);
-                
-                this.row += 1;                                                     ;
-            };
-            
-            this.onPlacement = new Event(this);
-            
-            board[0][4].color(colors[blocks[0]]);
-            board[1][4].color(colors[blocks[1]]);
-            board[2][4].color(colors[blocks[2]]);
-            
-            return this;
-        };
-        
-        Piece.colors = colors;
-        
-        Piece.isRoomAvailable = function isRoomAvailable(board) {
-            if(!board[0][4].isEmpty) { return false; }
-            if(!board[1][4].isEmpty) { return false; }
-            if(!board[2][4].isEmpty) { return false; }
-            
-            return true;
-        };
-        
-        return Piece;
-    })();
-
-    function compactBoard(board) {
-        var wasCompressed = false;
-        
-        for(var r = board.rows - 1; r > 0; r--) {
-            for(var c = 0; c < board.columns; c++) {
-                if(board[r][c].isEmpty && !board[r - 1][c].isEmpty) {
-                    board[r - 1][c].pos(r, c);
-                    wasCompressed = true;
-                }
-            }
-        }
-        
-        return wasCompressed;
-    }
-
-    function findMatches(board) {
-        var scores = [];
-        
-        for(var r = 0; r < board.rows; r++) {
-            for(var c = 0; c < board.columns; c++) {
-                var tests = [ [ board[r - 1][c], board[r][c], board[r + 1][c] ],
-                              [ board[r][c - 1], board[r][c], board[r][c + 1] ],
-                              [ board[r - 1][c - 1], board[r][c], board[r + 1][c  + 1] ],
-                              [ board[r + 1][c - 1], board[r][c], board[r - 1][c  + 1] ] ];
-                
-                for(var i = 0; i < tests.length; i++) {
-                    var score = true;
-                    
-                    if(tests[i][0].isEmpty) { continue; }
-                    
-                    for(var j = 0; j < tests[i].length - 1; j++) {
-                        if(tests[i][j].color() !== tests[i][j + 1].color()) { score = false; break; }
-                    }
-                    
-                    if(score) { Array.prototype.push.apply(scores, tests[i]); }
-                }
-            }
-        }
-        
-        for(var i = 0; i < scores.length; i++) { scores[i].destroy(); }
-        
-        return scores.length !== 0;
-    }
-    
-    return function Game(boardElem) {
-        var board = new Board(15, 9, boardElem);
-        
-        this.score = 0;
-        
-        this.colors = Piece.colors;
-        
-        this.piece = null;
-        
-        this.newPiece = function newPiece(blocks) {
-            this.piece = new Piece(board, blocks);
-            
-            var game = this;
-            this.piece.onPlacement.add(function() {
-                var loop = setInterval(function() {
-                    var wasCompressed = compactBoard(board);
-                    if(wasCompressed) { return; }
-                    
-                    var scored = findMatches(board);
-                    if(scored) { return; }
-                    
-                    game.onFreeze.trigger();
-                    clearInterval(loop);
-                }, 500);
-            });
-        };
-        
-        this.isGameOver = function isGameOver() {
-            return !Piece.isRoomAvailable(board);
-        };
-        
-        this.onFreeze = new Event(this);
-        
-        $(boardElem)
-            .css({ width: (20 * board.columns) + "px",
-                   height: (20 * board.rows) + "px" });
-    };
-})();
-
-(function() { // initialize
-    var game = new Game(document.getElementById("board"));
-    
-    $(document).keypress(function(e) {
-        var code = e.keyCode || e.which;
-            
-        if(!game.piece.isFalling) { return; }
-        
-        if(code === 37) { game.piece.left(); } // LEFT
-        else if(code === 38) { game.piece.rotate(); } // UP
-        else if(code === 39) { game.piece.right(); } // RIGHT
-        else if(code === 40) { game.piece.down(); } // DOWN
+        var pos = block.pos();
+        div.className = "block " + block.color();
+        div.style.top = pos.row * 20;
+        div.style.left = pos.col * 20;
+        block.board.div.appendChild(div);
     });
     
+    blk.Board.onCreate.add(function(board) {
+        board.div = document.createElement("div");
+        
+        $(board.div)
+            .addClass("board")
+            .css({ width: (20 * board.columns) + "px",
+                   height: (20 * board.rows) + "px" });
+    });
+
+    var game = new blk.Game();
+        
     game.onFreeze.add(function() {
         if(game.isGameOver()) { return; }
         
@@ -306,6 +69,20 @@ var Game = (function() {
         }, 500);
     });
     game.onFreeze.trigger();
+    
+    
+    $(document).keypress(function(e) {
+        var code = e.keyCode || e.which;
+            
+        if(!game.piece.isFalling) { return; }
+        
+        if(code === 37) { game.piece.left(); } // LEFT
+        else if(code === 38) { game.piece.rotate(); } // UP
+        else if(code === 39) { game.piece.right(); } // RIGHT
+        else if(code === 40) { game.piece.down(); } // DOWN
+    });
+    
+    $("#board-container").append(game.board.div);
 })();    
 </script>
 </body>

commit c6ddfb26b60dbf3420cd01c73f60dad312938c3a

Fixed bug that was accidentally introduced in 452781ec44a .  Also refactored chn.onCreate logic.

server.js
@@ -233,8 +233,9 @@ var chn = (function() {
 })();
 
 chn.onCreate(function(id, channel) {
-    if(id === "pictionary") { createBlockGame(channel); }
+    if(id === "pictionary") { createPictionary(channel); }
     else if(id === "tic-tac-toe") { createTicTacToeGame(channel); }
+    else if(id === "block") { createBlockGame(channel); }
 });
 
 function createBlockGame(channel) {
@@ -284,6 +285,12 @@ function createTicTacToeGame(channel) {
             var winner = game.isGameOver();
             if(winner) { sendMoreInfo("0", { "game-over": winner }); }
         }
+    });
+}
+
+function createPictionary(channel) {
+    channel.onReceive(function(msg) {
+        if("clear" in msg.content) { channel.data = channel.data.splice(-1); } 
     });
 }
 